1c08ad94781d335a343959aaf5118c17eab38b1c
kdow <kedownes@gmail.com>
2019-06-22 11:52:47 -0700

    Conditionally display most recent submission

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+
+  const displayRecentSub = props.recentSubmission !== ''
   return (
     <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{ props.recentSubmission }</p>
+      {
+        displayRecentSub && (
+          <div>
+            <h3>The Most Recent Submission</h3>
+            <p className="RecentSubmission__submission">{ props.recentSubmission }</p>
+          </div>
+        )
+      }
     </div>
   );
 }
